8c20a9c65d6f4333d8532713cf00e489f2d9513e
sansauc <santiagosaucedocrg@gmail.com>
2022-03-29 10:59:24 -0300

    solución inicial

---
+++ GestorTareas/GestorTareas/JMenu.Designer.cs
@@ -0,0 +1,209 @@
+﻿
+namespace GestorTareas {
+    partial class JMenu {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.archivoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBoxTareas = new System.Windows.Forms.ListBox();
+            this.txtTarea = new System.Windows.Forms.TextBox();
+            this.bttnAgregar = new System.Windows.Forms.Button();
+            this.bttnGuardar = new System.Windows.Forms.Button();
+            this.bttnEliminar = new System.Windows.Forms.Button();
+            this.bttnCancelar = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.archivoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(639, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // archivoToolStripMenuItem
+            // 
+            this.archivoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.salirToolStripMenuItem});
+            this.archivoToolStripMenuItem.Name = "archivoToolStripMenuItem";
+            this.archivoToolStripMenuItem.Size = new System.Drawing.Size(60, 20);
+            this.archivoToolStripMenuItem.Text = "Archivo";
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(96, 22);
+            this.salirToolStripMenuItem.Text = "Salir";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Image = global::GestorTareas.Properties.Resources.logo_b_1_300x85;
+            this.label1.Location = new System.Drawing.Point(0, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(639, 81);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(639, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Gestor de Tareas";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Tarea:";
+            // 
+            // listBoxTareas
+            // 
+            this.listBoxTareas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxTareas.FormattingEnabled = true;
+            this.listBoxTareas.Location = new System.Drawing.Point(17, 214);
+            this.listBoxTareas.Name = "listBoxTareas";
+            this.listBoxTareas.Size = new System.Drawing.Size(598, 199);
+            this.listBoxTareas.TabIndex = 4;
+            this.listBoxTareas.SelectedIndexChanged += new System.EventHandler(this.listBoxTareas_SelectedIndexChanged);
+            // 
+            // txtTarea
+            // 
+            this.txtTarea.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTarea.Location = new System.Drawing.Point(17, 179);
+            this.txtTarea.Name = "txtTarea";
+            this.txtTarea.Size = new System.Drawing.Size(395, 20);
+            this.txtTarea.TabIndex = 5;
+            this.txtTarea.TextChanged += new System.EventHandler(this.txtTarea_TextChanged);
+            // 
+            // bttnAgregar
+            // 
+            this.bttnAgregar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttnAgregar.Location = new System.Drawing.Point(429, 172);
+            this.bttnAgregar.Name = "bttnAgregar";
+            this.bttnAgregar.Size = new System.Drawing.Size(88, 32);
+            this.bttnAgregar.TabIndex = 6;
+            this.bttnAgregar.Text = "Nueva Tarea";
+            this.bttnAgregar.UseVisualStyleBackColor = true;
+            this.bttnAgregar.Click += new System.EventHandler(this.bttnAgregar_Click);
+            // 
+            // bttnGuardar
+            // 
+            this.bttnGuardar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttnGuardar.Location = new System.Drawing.Point(527, 172);
+            this.bttnGuardar.Name = "bttnGuardar";
+            this.bttnGuardar.Size = new System.Drawing.Size(88, 32);
+            this.bttnGuardar.TabIndex = 7;
+            this.bttnGuardar.Text = "Guardar";
+            this.bttnGuardar.UseVisualStyleBackColor = true;
+            this.bttnGuardar.Click += new System.EventHandler(this.bttnGuardar_Click);
+            // 
+            // bttnEliminar
+            // 
+            this.bttnEliminar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bttnEliminar.Location = new System.Drawing.Point(156, 419);
+            this.bttnEliminar.Name = "bttnEliminar";
+            this.bttnEliminar.Size = new System.Drawing.Size(88, 32);
+            this.bttnEliminar.TabIndex = 8;
+            this.bttnEliminar.Text = "Eliminar";
+            this.bttnEliminar.UseVisualStyleBackColor = true;
+            this.bttnEliminar.Click += new System.EventHandler(this.bttnEliminar_Click);
+            // 
+            // bttnCancelar
+            // 
+            this.bttnCancelar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bttnCancelar.Location = new System.Drawing.Point(429, 419);
+            this.bttnCancelar.Name = "bttnCancelar";
+            this.bttnCancelar.Size = new System.Drawing.Size(88, 32);
+            this.bttnCancelar.TabIndex = 9;
+            this.bttnCancelar.Text = "Cancelar";
+            this.bttnCancelar.UseVisualStyleBackColor = true;
+            this.bttnCancelar.Click += new System.EventHandler(this.bttnCancelar_Click);
+            // 
+            // JMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 450);
+            this.Controls.Add(this.bttnCancelar);
+            this.Controls.Add(this.bttnEliminar);
+            this.Controls.Add(this.bttnGuardar);
+            this.Controls.Add(this.bttnAgregar);
+            this.Controls.Add(this.txtTarea);
+            this.Controls.Add(this.listBoxTareas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "JMenu";
+            this.Text = "Menu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.JMenu_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem archivoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBoxTareas;
+        private System.Windows.Forms.TextBox txtTarea;
+        private System.Windows.Forms.Button bttnAgregar;
+        private System.Windows.Forms.Button bttnGuardar;
+        private System.Windows.Forms.Button bttnEliminar;
+        private System.Windows.Forms.Button bttnCancelar;
+    }
+}
+
---
+++ GestorTareas/GestorTareas/JMenu.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestorTareas {
+    public partial class JMenu : Form {
+
+        private bool tareaNueva;
+        private bool cambioTxBox;
+
+        public JMenu() {
+            InitializeComponent();
+            this.reset();
+        }
+    
+        //metodos de tareas
+
+        private void reset() {
+
+            this.bttnGuardar.Enabled = false;
+            this.bttnCancelar.Enabled = false;
+            this.bttnEliminar.Enabled = false;
+            this.listBoxTareas.Enabled = false;
+            this.txtTarea.Enabled = false;
+
+            //despues del guardar cambios
+            this.bttnAgregar.Enabled = true;
+
+            //Activa la lista solo si hay items en ella
+            this.listBoxTareas.Enabled = this.listBoxTareas.Items.Count > 0;
+
+            //quitar la selección de un elemento
+            this.listBoxTareas.SelectedIndex = -1;
+
+            //reseteo la bandera de la caja de texto
+            this.cambioTxBox = false;
+        }
+        
+
+        private void guardarCambios() {
+
+            string tarea = txtTarea.Text;
+
+            if (!string.IsNullOrEmpty(tarea)) {
+
+                if (this.tareaNueva == true) {
+
+                    this.listBoxTareas.Items.Add(tarea);
+
+                } else {
+
+                    this.listBoxTareas.Items[this.listBoxTareas.SelectedIndex] = tarea;
+
+                }
+
+                MessageBox.Show("Tarea almacenada exitosamente", "Atención");
+                this.txtTarea.Text = "";
+                this.reset();
+
+            } else {
+                MessageBox.Show("Debe ingresar una tarea antes de Guardar", "Atención");
+            }
+                
+        }
+
+        private void eliminarTarea() {
+
+
+
+            if (this.listBoxTareas.SelectedIndex >= 0 && this.listBoxTareas.SelectedIndex < this.listBoxTareas.Items.Count) {
+
+                if (MessageBox.Show("¿Desea eliminar la tarea?", "Mensaje de Error", MessageBoxButtons.YesNoCancel) == DialogResult.Yes) {
+
+                    this.listBoxTareas.Items.RemoveAt(this.listBoxTareas.SelectedIndex);
+                    this.txtTarea.Text = "";
+                    this.reset();
+
+                }
+            } else {
+                MessageBox.Show("No hay un elemento seleccionado", "Atención");
+            }
+        }
+
+        private void cancelar() {
+
+            if (this.cambioTxBox) {
+
+                if (MessageBox.Show("¿Desea guardar los cambios antes de salir?", "Atencion", MessageBoxButtons.YesNoCancel) == DialogResult.Yes) {
+
+                    this.guardarCambios();
+
+                }
+
+            }
+
+            this.txtTarea.Text = "";
+            this.reset();
+        }
+
+        private void editarItem() {
+
+            if (this.listBoxTareas.SelectedIndex >= 0 && this.listBoxTareas.SelectedIndex < this.listBoxTareas.Items.Count) {
+                string tareaSeleccionada = this.listBoxTareas.Items[listBoxTareas.SelectedIndex].ToString();
+                this.txtTarea.Text = tareaSeleccionada;
+
+                this.bttnGuardar.Enabled = true;
+                this.bttnEliminar.Enabled = true;
+                this.bttnCancelar.Enabled = true;
+                this.bttnAgregar.Enabled = false;
+                this.txtTarea.Enabled = true;
+
+                this.tareaNueva = false; //el booleano se utiliza para saber si es una tarea nueva o un change
+            }
+        }
+
+        // Metodos del Frame
+
+        private void bttnAgregar_Click(object sender, EventArgs e) {
+
+            if (this.cambioTxBox) {
+
+                if (MessageBox.Show("¿Guardar Cambios?", "Atencion", MessageBoxButtons.YesNoCancel) == DialogResult.Yes) {
+
+                    this.guardarCambios();
+
+                }
+
+            }
+
+            this.txtTarea.Enabled = true;
+            this.bttnGuardar.Enabled = true;
+            this.bttnEliminar.Enabled = true;
+            this.bttnCancelar.Enabled = true;
+            this.bttnAgregar.Enabled = false;
+            this.tareaNueva = true; //boleano indicando la carga de una nueva tarea
+            this.txtTarea.Focus(); //Posiciona el cursor en el jtext
+
+        }
+
+        private void bttnGuardar_Click(object sender, EventArgs e) {
+            this.guardarCambios();
+        }
+
+        private void bttnEliminar_Click(object sender, EventArgs e) {
+            this.eliminarTarea();
+        }
+
+        private void bttnCancelar_Click(object sender, EventArgs e) {
+            this.cancelar();
+        }
+
+        private void listBoxTareas_SelectedIndexChanged(object sender, EventArgs e) {
+
+            this.editarItem();
+
+        }
+
+        private void txtTarea_TextChanged(object sender, EventArgs e) {
+            this.cambioTxBox = true;
+        }
+
+        private void JMenu_FormClosing(object sender, FormClosingEventArgs e) {
+
+            DialogResult respuesta = MessageBox.Show("¿Guardar Cambios?", "Atencion", MessageBoxButtons.YesNoCancel);
+
+            if (respuesta == DialogResult.Yes) {
+
+                this.guardarCambios();
+                e.Cancel = true;
+                return;
+
+            }else if (respuesta == DialogResult.Cancel) {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
